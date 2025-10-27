e970e8e693d1d6cc69d4df98e8a28dae09535be8
stea7th <125774445+stea7th@users.noreply.github.com>
2025-10-26 18:15:57 -0700

    Update supabaseClient.ts

--- src/app/lib/supabaseClient.ts
+++ src/app/lib/supabaseClient.ts
@@ -1,12 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
+// src/lib/supabase/client.ts
+import { createBrowserClient } from '@supabase/ssr';
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  {
-    auth: {
-      persistSession: true,
-      autoRefreshToken: true,
-    },
-  }
-)
+export function createClient() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+}
