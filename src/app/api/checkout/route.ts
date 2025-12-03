beb5bbb191ce7fc00a83c5a36f3ce54094c00e98
stea7th <125774445+stea7th@users.noreply.github.com>
2025-12-03 09:54:53 -0800

    Update route.ts

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -39,7 +39,7 @@ export async function POST(req: NextRequest) {
       );
     }
 
-    const supabase = await createRouteClient(req);
+   const supabase = createRouteClient();
     const {
       data: { user },
       error: authError,
