73456f5c5c54a38c7fcacddb5bd5c0966e12d60e
stea7th <125774445+stea7th@users.noreply.github.com>
2025-12-29 15:40:49 -0800

    Update layout.tsx

--- src/app/lock/layout.tsx
+++ src/app/lock/layout.tsx
@@ -1,6 +1,10 @@
 // src/app/lock/layout.tsx
 import styles from "./lock.module.css";
 
-export default function LockLayout({ children }: { children: React.ReactNode }) {
+export default function LockLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return <div className={styles.lockRoot}>{children}</div>;
 }
