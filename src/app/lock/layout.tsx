508562dc283a5d7859570c9af75c2b97a0eaaaea
stea7th <125774445+stea7th@users.noreply.github.com>
2025-12-29 15:32:45 -0800

    Update layout.tsx

--- src/app/lock/layout.tsx
+++ src/app/lock/layout.tsx
@@ -1,4 +1,6 @@
 // src/app/lock/layout.tsx
+import "./lock.css";
+
 export default function LockLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return children;
 }
